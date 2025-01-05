28649b3e4c80c9f35897b0f9ac2610567f65de47
gudielmj <gudielmj@hotmail.com>
2025-01-05 20:58:23 +0100

    Add files via upload

---
+++ BuscaFotosRepetidas.py
@@ -0,0 +1,7 @@
+from Interfaz.FormatoVentana import *
+
+print ("Comentario")
+
+print("Aquí vamos a poner la funcionalidad de comparación de fotos")
+
+#LLamar a la ventana que se dibuja en el formato.
---
+++ Interfaz/FormatoVentana.py
@@ -0,0 +1,8 @@
+class PintaVentana():
+
+	print ("Aquí se construye la ventana")
+
+	print("Aquí montaremos todo lo relacionado con la ventana")
+
+#Hay que importar el módulo tkinter
+#Y dibujar la ventana
