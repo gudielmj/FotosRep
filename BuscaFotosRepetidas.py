a63210fd834cfad9064ff14dfa8ba1bf44e19534
gudielmj <gudielmj@hotmail.com>
2025-01-07 18:35:22 +0100

    Update BuscaFotosRepetidas.py

--- BuscaFotosRepetidas.py
+++ BuscaFotosRepetidas.py
@@ -1,5 +1,7 @@
 from Interfaz.FormatoVentana import *
 
+#Una vez que creamos la ventana, creamos la funcionalidad
+
 print ("Comentario")
 
 print("Aquí vamos a poner la funcionalidad de comparación de fotos")
