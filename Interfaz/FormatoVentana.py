285fd0b8146d29a88ff365595e83acff72814dad
gudielmj <gudielmj@hotmail.com>
2025-01-07 19:04:11 +0100

    git

--- Interfaz/FormatoVentana.py
+++ Interfaz/FormatoVentana.py
@@ -10,6 +10,8 @@ class PintaVentana():
     
     root.mainloop()
     
+    #Ya hemos dibujado la pantalla sin campos
+    
 
 #Hay que importar el módulo tkinter
 #Y dibujar la ventana para poderlo llamar desde el otro archivo
